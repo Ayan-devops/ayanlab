56e3f6ca30f6d03bf0b9f06dd28e45bb5f2c9f56
Ayan-devops <sayyedayan92@gmail.com>
2021-09-06 00:43:56 +0530

    second commit

---
+++ lab/src/main/java/my/lab/lab/RestControllerAdvice.java
@@ -0,0 +1,26 @@
+package my.lab.lab;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@org.springframework.web.bind.annotation.RestControllerAdvice
+public class RestControllerAdvice {
+
+	@ExceptionHandler(value=Exception.class)
+	public void exceptionHandler(Exception exception)
+	{
+		System.out.println("*******");
+	}
+	
+	@ExceptionHandler(value=NullPointerException.class)
+	public void exceptionHandler1(NullPointerException exception)
+	{
+		System.out.println("*******");
+	}
+	
+	@ExceptionHandler(value=MethodArgumentNotValidException.class)
+	public String exceptionHandler1(MethodArgumentNotValidException exception)
+	{
+		return "Value cannot be null";
+	}
+}
